47fa15b12ef84023abd63ddb56b4ad722f933a5d
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2022-05-21 17:56:32 +0000

    Made validations for broken urls, also check if http is on the url, if not will add it.

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import requests
 
-url = input("Informe uma URL's para verificar separadas por virgula: ")
-url.split(',')
-url_list = [url]
+print("Welcome to the Website Status Checker!")
+option = ''
+while option != 'n':
+    if option != 'n':
+        urls = input("What URL you want to check? (Use commas to separate them): ").lower()
+        urls_list = urls.split(',')
+        
+        
+        for url in urls_list:
+            try:
+                if "." not in url:
+                    print(f'Invalid URL: "{url}"')
+                    break
+                if "http" not in url:
+                    url = "http://"+url.strip()
+                       
+                url_request = requests.get(url)
+                if url_request.status_code == 200:
+                    print(f"\n{url.strip()} website is online!")
+                
+            except requests.exceptions.RequestException:
+                print(f"\n{url} website is offline")
+        
+    
+    while option != 'n' or option != 'y':
+        option = input("Do you wish to check another website? (y/n): ").lower()
 
-print(url)
+        if option == 'n':
+            print("Thank you, see you next time!")
+            break
+        elif option == 'y':
+            break
+        else:
+            print("invalid option")
+        
+    
